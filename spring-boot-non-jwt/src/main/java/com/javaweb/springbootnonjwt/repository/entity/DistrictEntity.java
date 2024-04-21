c353b26acc3dd8797a00dca107647350fef6a0fc
abcxyz443 <bgfcvt@gmail.com>
2024-04-21 13:06:36 +0700

    update project

--- spring-boot-non-jwt/src/main/java/com/javaweb/springbootnonjwt/repository/entity/DistrictEntity.java
+++ spring-boot-non-jwt/src/main/java/com/javaweb/springbootnonjwt/repository/entity/DistrictEntity.java
@@ -1,8 +1,7 @@
 package com.javaweb.springbootnonjwt.repository.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.util.ArrayList;
 import java.util.List;
